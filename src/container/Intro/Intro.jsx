b5b905ef4f81e46af88acad0f4c32275960f43d7
rodrifernan <rodrifernandez8@gmail.com>
2022-06-14 20:30:59 -0300

    Video terminado

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+//import {'BsPauseFill', 'BsFillPlayFill'} from "react-icons"
+import { meal } from "../../constants";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import "./Intro.css";
+
+const Intro = () => {
+  const vidref = React.useRef();
+  const [playVideo, setPlayVideo] = useState(false);
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      vidref.current.pause();
+    } else {
+      vidref.current.play();
+    }
+  };
+
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        type="video/mp4"
+        ref={vidref}
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
